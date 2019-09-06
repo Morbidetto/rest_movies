31cb61480ab3ccd34959c2bf343fe5cff21f444e
Jakub Ledwon <jakub.ledwon@nokia.com>
2019-09-06 15:58:32 +0200

    Views and serializers template

--- src/movies/views.py
+++ src/movies/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+import requests
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+frrom .serializers import CreateMovieSerializer
 
-# Create your views here.
+
+@api_view(['GET'])
+def external_moviedb_view(request):
+    if request.method == 'GET':
+        title = request.data['title']
+        r = requests.get('http://www.omdbapi.com/?apikey={key}&t={title}'.format(key, title))
+        if r.status_code == 200:
+                data = r.json()
+                return Response(data, status=status.HTTP_200_OK)
+        return Response({"error": "Request failed"}, status=r.status_code)
+
+
+class MoviesView(APIView):
+
+    def get(self, request):
+        pass
+
+    def post(self,request):
+        serializer = CreateMovieSerializer(data=request.data)
+        if serializer.is_valid():
+            external_response = external_moviedb_view(request)
+            serializer = 
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
